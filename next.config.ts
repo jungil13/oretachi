223e63457262c27e5aca2e4f93df1dc3f6d328e5
jungil13 <jungilcasquejo5@gmail.com>
2026-07-11 12:20:05 +0800

    image fix

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["lucide-react", "recharts"],
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "dysmgagcfzetasefbjxb.supabase.co" },
